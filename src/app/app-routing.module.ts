0842c7dab59151bd4d1aebe4ff623d2b290c46e6
Pere <frederic_pere@hotmail.com>
2020-05-14 18:00:15 +0200

    first chart added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 
 const routes: Routes = [{
@@ -10,7 +11,12 @@ const routes: Routes = [{
   children: [{
     path: '',
     component: DashboardComponent
-  }]
+            },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+  ]
 }];
 
 @NgModule({
